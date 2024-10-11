fd2876f86bccfa36bb042f3e406c2354073c0e16
Maiia11 <149806655+Maiia11@users.noreply.github.com>
2024-10-11 13:12:40 +0300

    end home page

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -6,7 +6,7 @@ const HomePage = () => {
           <div className={css.title}>
           <h1 className={css.heroText}>Campers of your dreams</h1>
           <p className={css.heroParagraph}> You can find everything you want in our catalog</p>
-          <button> View Now</button>
+          <button className={css.btn}> View Now</button>
           </div>
     </div>
   )
