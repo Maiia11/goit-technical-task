dccff4844395a5301eb48a50b6c324ece872a0d4
Maiia11 <149806655+Maiia11@users.noreply.github.com>
2024-10-12 22:40:00 +0300

    start style checkbox

--- src/components/FilterForm/FilterForm.jsx
+++ src/components/FilterForm/FilterForm.jsx
@@ -2,6 +2,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { selectFilters } from "../../redux/selectors";
 import { chooseLocation, setEquipmentFilter } from "../../redux/filtersSlice";
 import { Field, Form, Formik } from "formik";
+import css from './FilterForm.module.css'
 
 const FilterForm = () => {
     const dispatch = useDispatch();
@@ -26,57 +27,76 @@ const FilterForm = () => {
             {() => {
                 return (
                     <Form>
-                        <Field
+                        {/* Location */}
+                        
+                        <div className={css.formLocation}>
+                        <h3 className={css.locationLabel}>Location</h3>
+                        <Field className={css.locationInput}
                             name="location"
-                            placeholder="Location"
+                            placeholder="name of the city"
                             value={filters.location}
                             onChange={handleLocationChange}
-                        />
-                        <label>
-                            <Field
-                                type="checkbox"
-                                name="AC"
-                                checked={filters.equipment.ac}
-              onChange={handleCheckboxChange}
                             />
-                            AC
-                        </label>
-                        <label>
-                            <Field
-                                type="checkbox"
-                                name="transmission"
-                                checked={filters.equipment.transmission === "automatic"}
-              onChange={handleCheckboxChange}
-                            />
-                            Automatic
-                        </label>
-                        <label>
-                            <Field
-                                type="checkbox"
-                                name="kitchen"
-                                checked={filters.equipment.kitchen}
-              onChange={handleCheckboxChange}
-                            />
-                            Kitchen
-                        </label>
-                        <label>
-                            <Field
-                                type="checkbox"
-                                name="TV"
-                               checked={filters.equipment.tv}
-              onChange={handleCheckboxChange}
-                            />
-                            TV
-                        </label>
-                        <label>
-                            <Field
-                                type="checkbox"
-                                name="bathroom"
-                                checked={filters.equipment.bathroom}
-              onChange={handleCheckboxChange}
-                            />
-                            Bathroom
-                        </label>
+                        </div>
+                        
+                        {/*  Filters*/}
+                        
+                        <h3 className={css.title}>Filters</h3>
+
+                        <div className={css.containerCheckbox}>
+                            <h4 className={css.titleCheckbox}>Vehicle equipment</h4>
+                            
+                            <div className={css.checkbox}>
+                                <label className={css.checkboxLabel}>
+                                    <Field className={css.checkboxInput}
+                                        type="checkbox"
+                                        name="AC"
+                                        checked={filters.equipment.ac}
+                                        onChange={handleCheckboxChange}
+                                    />
+                                    <span className={css.customCheckbox}>AC</span> {/* Текст внутри чекбокса */}
+                                </label>
+
+                                <label className={css.checkboxLabel}>
+                                    <Field className={css.checkboxInput}
+                                        type="checkbox"
+                                        name="transmission"
+                                        checked={filters.equipment.transmission === "automatic"}
+                                        onChange={handleCheckboxChange}
+                                    />
+                                    <span className={css.customCheckbox}>Automatic</span>
+                                </label>
+                                
+                                <label className={css.checkboxLabel}>
+                                    <Field className={css.checkboxInput}
+                                        type="checkbox"
+                                        name="kitchen"
+                                        checked={filters.equipment.kitchen}
+                                        onChange={handleCheckboxChange}
+                                    />
+                                    <span className={css.customCheckbox}>Kitchen</span>
+                                </label>
+                                <label className={css.checkboxLabel}>
+                                    <Field className={css.checkboxInput}
+                                        type="checkbox"
+                                        name="TV"
+                                        checked={filters.equipment.tv}
+                                        onChange={handleCheckboxChange}
+                                    />
+                                    <span className={css.customCheckbox}>TV</span>
+                                </label>
+                                
+                                <label className={css.checkboxLabel}>
+                                    <Field className={css.checkboxInput}
+                                        type="checkbox"
+                                        name="bathroom"
+                                        checked={filters.equipment.bathroom}
+                                        onChange={handleCheckboxChange}
+                                    />
+                                    <span className={css.customCheckbox}>Bathroom</span>
+                                </label>
+                            </div>
+                        </div>
                     </Form>
                 );
             }}
