dd57ddbfc36e899ee6eb698b67c8d5609c8379b3
Maiia11 <149806655+Maiia11@users.noreply.github.com>
2024-10-12 19:26:50 +0300

    Update FilterForm.jsx

--- src/components/FilterForm/FilterForm.jsx
+++ src/components/FilterForm/FilterForm.jsx
@@ -1,16 +1,12 @@
-import { Field, Form, Formik } from "formik"
-import { useEffect, useId, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { selectFilters } from "../../redux/selectors";
+import { useState } from "react";
 import { chooseLocation, setEquipmentFilter } from "../../redux/filtersSlice";
-import css from "./FilterForm.module.css"
-
-
+import { Field, Form, Formik } from "formik";
 
 const FilterForm = () => {
     const dispatch = useDispatch();
     const filter = useSelector(selectFilters);
-   
     
     const [formValues, setFormValues] = useState({
         location: filter.location || "",
@@ -21,19 +17,29 @@ const FilterForm = () => {
         bathroom: filter.equipment.bathroom || false
     });
 
-    
-    
-    // const initialValues = {
-    //     location: filter.location || "",
-    //     ac: filter.equipment.ac || false,
-    //     automatic: filter.equipment.automatic || false,
-    //     kitchen: filter.equipment.kitchen || false,
-    //     tv: filter.equipment.tv || false,
-    //     bathroom: filter.equipment.bathroom || false,
-    // };
-   
+    // Обновляем фильтры в Redux при изменении формы
+    const handleChange = (e) => {
+        const { name, value, type, checked } = e.target;
+        const newValue = type === 'checkbox' ? checked : value;
+
+        setFormValues(prevValues => ({
+            ...prevValues,
+            [name]: newValue
+        }));
+
+        // Обновляем фильтры в Redux при каждом изменении
+        if (name === "location") {
+            dispatch(chooseLocation(newValue));
+        } else {
+            dispatch(setEquipmentFilter({
+                ...formValues,
+                [name]: newValue
+            }));
+        }
+    };
+
     const handleSubmit = (values) => {
-        console.log("Submitted values:", values);
+        // Обрабатываем отправку формы
         dispatch(chooseLocation(values.location));
         dispatch(setEquipmentFilter({
             ac: values.ac,
@@ -44,75 +50,64 @@ const FilterForm = () => {
         }));
     };
 
-     useEffect(() => {
-        setFormValues({
-            location: filter.location || "",
-            ac: filter.equipment.ac || false,
-            automatic: filter.equipment.automatic || false,
-            kitchen: filter.equipment.kitchen || false,
-            tv: filter.equipment.tv || false,
-            bathroom: filter.equipment.bathroom || false
-        });
-    }, [filter]);
-
-
-    const locationFieldId = useId();
     return (
-      <>
-            <Formik initialValues={formValues} onSubmit={handleSubmit}>
-                
-                <Form className={css.formLocation}>
-                    {/* location */}
-                    <label htmlFor={locationFieldId} className={css.locationLabel}>Location</label>
-                    <Field className={css.locationInput} type="text" name="location" id={locationFieldId}  placeholder="Name of the city"/>
-                
-                    <h4 className={css.title}>Filters</h4>
+        <Formik
+            initialValues={formValues}
+            onSubmit={handleSubmit}
+        >
+            {() => {
+                return (
+                    <Form>
+                        <Field
+                            name="location"
+                            placeholder="Location"
+                            onChange={handleChange}
+                        />
+                        <label>
+                            <Field
+                                type="checkbox"
+                                name="ac"
+                                onChange={handleChange}
+                            />
+                            AC
+                        </label>
+                        <label>
+                            <Field
+                                type="checkbox"
+                                name="automatic"
+                                onChange={handleChange}
+                            />
+                            Automatic
+                        </label>
+                        <label>
+                            <Field
+                                type="checkbox"
+                                name="kitchen"
+                                onChange={handleChange}
+                            />
+                            Kitchen
+                        </label>
+                        <label>
+                            <Field
+                                type="checkbox"
+                                name="tv"
+                                onChange={handleChange}
+                            />
+                            TV
+                        </label>
+                        <label>
+                            <Field
+                                type="checkbox"
+                                name="bathroom"
+                                onChange={handleChange}
+                            />
+                            Bathroom
+                        </label>
+                    </Form>
+                );
+            }}
+        </Formik>
+    );
+};
 
-                    {/*Vehicle equipment  */}
-                    <h5>Vehicle equipment</h5>
-                     
-                    <div className="vehicle-equipment">
-          <label className="equipment-item">
-            <Field type="checkbox" name="ac" />
-            <div className="label">
-              <p>AC</p>
-            </div>
-          </label>
-
-          <label className="equipment-item">
-            <Field type="checkbox" name="automatic" />
-            <div className="label">
-              <p>Automatic</p>
-            </div>
-          </label>
-
-          <label className="equipment-item">
-            <Field type="checkbox" name="kitchen" />
-            <div className="label">
-              <p>Kitchen</p>
-            </div>
-          </label>
-
-          <label className="equipment-item">
-            <Field type="checkbox" name="tv" />
-            <div className="label">
-              <p>TV</p>
-            </div>
-          </label>
-
-          <label className="equipment-item">
-            <Field type="checkbox" name="bathroom" />
-            <div className="label">
-              <p>Bathroom</p>
-            </div>
-          </label>
-        </div>
-
-                </Form>
-            </Formik>
-            
-        </>
-  )
-}
-
-export default FilterForm
+export default FilterForm;
