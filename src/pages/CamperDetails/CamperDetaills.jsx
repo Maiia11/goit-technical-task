864d10782273ebecbfa4caa3635c750970181950
Maiia11 <149806655+Maiia11@users.noreply.github.com>
2024-10-13 19:20:26 +0300

    style details

--- src/pages/CamperDetails/CamperDetaills.jsx
+++ src/pages/CamperDetails/CamperDetaills.jsx
@@ -5,6 +5,7 @@ import { useEffect, useState } from "react";
 import Features from "../../components/Featers/Featers";
 import Reviews from "../../components/Reviews/Reviews";
 import BookingForm from "../../components/BookingForm/BookingForm";
+import css from "./CamperDetails.module.css"
 
 
 const CamperDetaills = () => {
@@ -26,28 +27,36 @@ const CamperDetaills = () => {
         return <p>Loading...</p>
     }
 
-  return (
-     <div>
-          <h2>{camper.name}</h2>
-          <p>{`${camper.rating} (${camper.reviews.length} Reviews)`}</p>
+    return (
+      
+     <div className={css.container}>
+          <div>
+                <h2 className={css.tittle}>{camper.name}</h2>
+                <div className={css.details}>
+          <p className={css.reviews}>{`${camper.rating} (${camper.reviews.length} Reviews)`}</p>
           <p>{camper.location}</p>
-          <p>{camper.price}</p>
+                </div>
+                <p className={css.price}>{camper.price}</p>
+            </div>
 
+            <div className={css.imgContainer}>
           {camper.gallery.map((image, index) => (
-              <img key={index} src={image.original} alt={`Gallery image ${index + 1}`} />
+              <img className={css.img} key={index} src={image.original} alt={`Gallery image ${index + 1}`} />
           ))}
+            </div>
 
-            <p> {camper.description}</p>
+            <p className={css.description}> {camper.description}</p>
             
           <div>
-              <button onClick={() => setActiveTab('features')}>Features</button>
-        <button onClick={() => setActiveTab('reviews')}>Reviews</button>
+                <button className={css.btn} onClick={() => setActiveTab('features')}>Features</button>
+        <button className={css.btn} onClick={() => setActiveTab('reviews')}>Reviews</button>
           </div>
           
           {camper && activeTab === 'features' ? <Features camper={camper} /> : <Reviews camper={camper} />}
 
-          <BookingForm />
-        </div>
+                <BookingForm />
+            
+    </div>
   )
 }
 
