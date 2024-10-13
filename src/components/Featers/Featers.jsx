38d4159e97428d812816039d971b2f673e16d62d
Maiia11 <149806655+Maiia11@users.noreply.github.com>
2024-10-13 20:07:16 +0300

    featers style

--- src/components/Featers/Featers.jsx
+++ src/components/Featers/Featers.jsx
@@ -1,46 +1,76 @@
-import css from './Featers.module.css'
+import css from "./Featers.module.css";
 
 const Features = ({ camper }) => {
-    console.log(camper);
-    const { transmission, engine, kitchen, AC, bathroom, TV, radio, refrigerator, microwave, gas, water
+  console.log(camper);
+  const {
+    transmission,
+    engine,
+    kitchen,
+    AC,
+    bathroom,
+    TV,
+    radio,
+    refrigerator,
+    microwave,
+    gas,
+    water,
+    form,
+    length,
+    width,
+    height,
+    tank,
+    consumption,
+  } = camper;
 
- } = camper;
-    
   return (
     <div className={css.container}>
       <div className={css.bagesContainer}>
-                      <p className={css.bages}>{transmission}</p>
-                      <p className={css.bages}>{engine}</p>
-                      {kitchen && <p className={css.bages}> Kitchen </p>}
+        <p className={css.bages}>{transmission}</p>
+        <p className={css.bages}>{engine}</p>
+        {kitchen && <p className={css.bages}> Kitchen </p>}
         {AC && <p className={css.bages}>AC</p>}
         {bathroom && <p className={css.bages}>Bathroom</p>}
         {TV && <p className={css.bages}>TV</p>}
-        {radio
- && <p className={css.bages}>Radio
-          </p>}
-        {refrigerator
- && <p className={css.bages}>Refrigerator
-          </p>}
-        {microwave
- && <p className={css.bages}>Microwave
-          </p>}
-        {gas
- && <p className={css.bages}>gas
-          </p>}
-        {water
- && <p className={css.bages}>water
-          </p>}
-        
-        
-        
-                  </div>
-            {/* <div>
-                {transmission && <p>Transmission: {transmission}</p>}
-                {engine && <p>Engine: {engine}</p>}
-                {kitchen && <p>Kitchen: Kitchen</p>}
-                {AC && <p>AC: Air Conditioning</p>}
-            </div> */}
+        {radio && <p className={css.bages}>Radio</p>}
+        {refrigerator && <p className={css.bages}>Refrigerator</p>}
+        {microwave && <p className={css.bages}>Microwave</p>}
+        {gas && <p className={css.bages}>gas</p>}
+        {water && <p className={css.bages}>water</p>}
+      </div>
+
+      <div>
+        <h3 className={css.titleDetails}>Vehicle details</h3>
+        <div className={css.details}>
+          <p>Form</p>
+          <p>{form}</p>
         </div>
+
+        <div className={css.details}>
+          <p>Length</p>
+          <p>{length}</p>
+        </div>
+
+        <div className={css.details}>
+          <p>Width</p>
+          <p>{width}</p>
+        </div>
+
+        <div className={css.details}>
+          <p>Height</p>
+          <p>{height}</p>
+        </div>
+
+        <div className={css.details}>
+          <p>Tank</p>
+          <p>{tank}</p>
+        </div>
+
+        <div className={css.details}>
+          <p>Consumption</p>
+          <p>{consumption}</p>
+        </div>
+      </div>
+    </div>
   );
 };
 
