ba0c32da14ccc4c65d3fc99ef081e7ffb98f10c2
Maiia11 <149806655+Maiia11@users.noreply.github.com>
2024-10-14 14:40:44 +0300

    Update Catalog.jsx

--- src/pages/Catalog/Catalog.jsx
+++ src/pages/Catalog/Catalog.jsx
@@ -3,26 +3,42 @@ import CatalogList from "../../components/CatalogList/CatalogList";
 import FilterForm from "../../components/FilterForm/FilterForm";
 import { filteredCampers } from "../../redux/selectors";
 import css from "./Catalog.module.css"
+import { useState } from "react";
 
 export const Catalog = () => {
     const campers = useSelector(filteredCampers);
-    console.log(campers);
     
 
+    const [itemsPerPage, setItemsPerPage] = useState(3); // Количество отображаемых кемперов
+    const [currentPage, setCurrentPage] = useState(0); // Текущая страница
+    
+console.log(campers);
     if (!Array.isArray(campers) || campers.length === 0) {
         return <p>No cars available</p>;
     }
+
+    const startIndex = currentPage * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+  const displayedCampers = campers.slice(startIndex, endIndex);
+
+  const loadMoreCampers = () => {
+    setCurrentPage((prevPage) => prevPage + 1);
+    };
+    
   return (
       <div className={css.container}>
           <FilterForm />
           <div className={css.containerCatalog}>
-          <ul>{campers.map((camper) => {
+          <ul>{displayedCampers.map((camper) => {
               return (<li key={camper.id}>
                   <CatalogList camper={camper} />
               </li>)
              })}   
           </ul> 
           </div>
+           <button onClick={loadMoreCampers}>
+            Load More
+          </button>
         </div>
   )
 }
